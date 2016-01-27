2adb8b354b6dd8ea51eb00407ea6ba57169c31db
陈欣 <95323362@qq.com>
2016-01-27 21:24:42 +0800

    数据结构里的英文

---
+++ 2.Programming in Objective-C.m
@@ -0,0 +1,24 @@
+dive right in 				直接切入主题
+Without further ado 		无需大费周折
+sophisticated 				老于世故的、功能齐全的
+sorting 					排序	
+Bitmask 					位屏蔽
+Linked List 				链表
+Hash Table 					哈希表
+Binary Heap				 	二叉堆
+Binary Search Tree 			二叉搜索树
+Graph Structures 			图结构
+Union-Find DS 				并查集
+Segment Tree 				线段树
+Fenwick Tree 				树状数组 
+Recursion Tree 				递归树 
+Graph Traversal 			图遍历
+Min Spanning Tree 			最小生成树
+SS Shortest path 			最短路径
+Network Flow 				网络流 
+Graph Matching 				图匹配
+Cycle Finding 				循环
+Suffix Tree 				后缀树
+Suffix Array 				后缀数组
+Geometry 					几何
+
