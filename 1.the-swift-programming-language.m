215ce61555efff75a6ffc851ed8370630291323c
陈欣 <95323362@qq.com>
2016-02-04 01:34:41 +0800

    1.the-swift-programming-language.m

---
+++ 1.the-swift-programming-language.m
@@ -0,0 +1,111 @@
+ builds on the best of C and Objective-C
+ 结合了C和Objective-C的优点
+
+ compatibility
+ 兼容性
+
+ adopts safe programming pattern
+ 采用安全的编程模式
+
+ collection literals
+ 集合语法
+
+ parameters
+ 参数
+
+ the procedural and object-oriented portions of the language
+ 过程式编程和面向对象编程
+
+ an innovative feature
+ 一个革命性的特性
+
+ optimized for performance
+ 对性能进行了优化
+
+ without compromising on either
+ 两者互不干扰,鱼与熊掌兼得
+
+ scale from ... to ...
+ 规模从...到...
+
+ continue to evolve with new features and capabilities
+ 伴随着新的特性和功能持续演进
+
+ A Swift Tour
+ swift初见
+
+ import a separate library
+ 导入一个单独的库
+
+ global scope 
+ 全局作用域
+
+ 编程例子
+ programming tasks
+
+ 在每个语句结尾写上分号
+ write semicolons at the end of every statement
+
+ 显式
+ explicitly
+
+ 永远不会被隐式转换为其他类型
+ never implicitly converted to another type
+
+ 用冒号分割
+ separated by a colon
+
+ 赋值
+ assign
+
+ Write the value in parentheses
+ 把值写到括号中
+
+ write a backslash
+ 写一个反斜杠
+
+ Create arrays and dictionaries using brackets ([])
+ 使用方括号 [] 来创建数组和字典
+
+ index
+ 下标
+
+ initializer syntax
+ 初始化语法
+
+ 数组和字典的初始化方法：
+ let emptyArray = String[]()
+ let emptyDictionary = Dictionary<String, Float>()
+
+pass an argument to a function
+给函数传参数
+
+Parentheses around the condition or loop variable are optional
+包裹条件和循环 变量括号可以省略
+
+Braces around the body are required
+但是语句体的大括号是必须的
+
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
+
+You can use if and let together to work with values that might be missing. These values are represented as optionals. An optional value either contains a value or contains nil to indicate that the value is missing. Write a question mark (?) after the type of a value to mark the value as optional. 
+你可以一起使用 if 和 let 来处理值缺失的情况。这些值可由可选值来代表。一个可选的值是一个具体的值或者 是 nil 以表示值缺失。在类型后面加一个问号来标记这个变量的值是可选的。 
+
+suffix
+后缀
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+
+
+
+
+
